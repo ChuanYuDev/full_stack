6f9466653d1e08eebe194fc3e1c8ed6518d59748
nealmatrix <nealmatrixgg@gmail.com>
2025-07-21 15:15:12 -0700

    fix SalesController

--- asp.net_core_mvc/frank_tutorial/WebAppMVC/Controllers/SalesController.cs
+++ asp.net_core_mvc/frank_tutorial/WebAppMVC/Controllers/SalesController.cs
@@ -1,56 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
+using UseCases.CategoriesUseCases;
+using UseCases.ProductsUseCases;
 using WebAppMVC.ViewModels;
-using WebAppMVC.Models;
 
 namespace WebAppMVC.Controllers
 {
     public class SalesController : Controller
     {
+        private readonly ISellProductUseCase sellProductUseCase;
+        private readonly IViewCategoriesUseCase viewCategoriesUseCase;
+        private readonly IViewProductsInCategoryUseCase viewProductsInCategoryUseCase;
+        private readonly IViewSelectedProductUseCase viewSelectedProductUseCase;
+
+        public SalesController(
+            ISellProductUseCase sellProductUseCase,
+            IViewCategoriesUseCase viewCategoriesUseCase,
+            IViewProductsInCategoryUseCase viewProductsInCategoryUseCase,
+            IViewSelectedProductUseCase viewSelectedProductUseCase
+        )
+        {
+            this.sellProductUseCase = sellProductUseCase;
+            this.viewCategoriesUseCase = viewCategoriesUseCase;
+            this.viewProductsInCategoryUseCase = viewProductsInCategoryUseCase;
+            this.viewSelectedProductUseCase = viewSelectedProductUseCase;
+        }
+
         public IActionResult Index()
         {
             var salesViewModel = new SalesViewModel
             {
-                Categories = CategoriesRepository.GetCategories()
+                // Categories = CategoriesRepository.GetCategories()
+                Categories = viewCategoriesUseCase.Execute()
             };
 
             return View(salesViewModel);
-        }
-
-        public IActionResult SellProductPartial(int productId)
-        {
-            var product = ProductsRepository.GetProductById(productId);
-
-            return PartialView("_SellProduct", product);
-        }
-
-        public IActionResult Sell(SalesViewModel salesViewModel)
-        {
-            var product = ProductsRepository.GetProductById(salesViewModel.SelectedProductId);
-
-            if (ModelState.IsValid)
-            {
-                // Sell the product    
-                if (product != null)
-                {
-                    TransactionsRepository.Add(
-                        productId: salesViewModel.SelectedProductId,
-                        productName: product.Name,
-                        price: product.Price.HasValue ? product.Price.Value : 0,
-                        beforeQty: product.Quantity.HasValue ? product.Quantity.Value : 0,
-                        soldQty: salesViewModel.QuantityToSell,
-                        cashierName: "Cashier1"
-                    );
-
-                    product.Quantity -= salesViewModel.QuantityToSell;
-                    ProductsRepository.UpdateProduct(salesViewModel.SelectedProductId, product);
-                }
-            }
-
-            // Get SelectedCategoryId by SelectProductId
-            salesViewModel.SelectedCategoryId = (product?.CategoryId) ?? 0;
-
-            salesViewModel.Categories = CategoriesRepository.GetCategories();
-            return View("Index", salesViewModel);
         }
 
         // Partial
@@ -60,10 +43,57 @@ namespace WebAppMVC.Controllers
             // Test-driven development (TDD)
             //      Write a test case first and make it fail
             //      Make a test case work later
-            var products = ProductsRepository.GetProductsByCategoryId(categoryId);
+
+            // var products = ProductsRepository.GetProductsByCategoryId(categoryId);
+            var products = viewProductsInCategoryUseCase.Execute(categoryId);
 
             // _Products: partial view name
             return PartialView("_Products", products);
+        }
+
+        public IActionResult SellProductPartial(int productId)
+        {
+            // var product = ProductsRepository.GetProductById(productId);
+            var product = viewSelectedProductUseCase.Execute(productId);
+
+            return PartialView("_SellProduct", product);
+        }
+
+        public IActionResult Sell(SalesViewModel salesViewModel)
+        {
+            // var product = ProductsRepository.GetProductById(salesViewModel.SelectedProductId);
+
+            if (ModelState.IsValid)
+            {
+                // Sell the product    
+                // if (product != null)
+                // {
+                //     TransactionsRepository.Add(
+                //         productId: salesViewModel.SelectedProductId,
+                //         productName: product.Name,
+                //         price: product.Price.HasValue ? product.Price.Value : 0,
+                //         beforeQty: product.Quantity.HasValue ? product.Quantity.Value : 0,
+                //         soldQty: salesViewModel.QuantityToSell,
+                //         cashierName: "Cashier1"
+                //     );
+
+                //     product.Quantity -= salesViewModel.QuantityToSell;
+                //     ProductsRepository.UpdateProduct(salesViewModel.SelectedProductId, product);
+                // }
+
+                sellProductUseCase.Execute("Cashier1", salesViewModel.SelectedProductId, salesViewModel.QuantityToSell);
+
+            }
+
+            var product = viewSelectedProductUseCase.Execute(salesViewModel.SelectedProductId);
+
+            // Get SelectedCategoryId by SelectProductId
+            salesViewModel.SelectedCategoryId = (product?.CategoryId) ?? 0;
+
+            // salesViewModel.Categories = CategoriesRepository.GetCategories();
+            salesViewModel.Categories = viewCategoriesUseCase.Execute();
+
+            return View("Index", salesViewModel);
         }
     }
 }
