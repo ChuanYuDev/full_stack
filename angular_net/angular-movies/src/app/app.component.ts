e92d5e5226e3b49825fe58730d25abd4ec3f6afb
nealmatrix <nealmatrixgg@gmail.com>
2025-08-19 07:43:34 +0000

    23 Pipes

--- angular_net/angular-movies/src/app/app.component.ts
+++ angular_net/angular-movies/src/app/app.component.ts
@@ -1,21 +1,26 @@
+import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // From Angular 19 onwards, `standalone: true` is not necessary in component decorator, since components are standalone by default
+
+  // Because of standalone application, we need to list the dependencies of the object in the object itself
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'this is a custom title';
+  // title = 'this is a custom title';
+
+  // duplicateNumber(value: number): number {
+  //   return value * 2;
+  // }
 
   movie = {
     title: 'Spider-Man',
-    releaseDate: new Date()
-  }
-
-  duplicateNumber(value: number): number {
-    return value * 2;
+    releaseDate: new Date(),
+    price: 1400.99 
   }
 }
