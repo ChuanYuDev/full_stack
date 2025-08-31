1f9a5d391659e8e5ef146a2bec7759ea6684dcf2
nealmatrix <nealmatrixgg@gmail.com>
2025-08-31 00:11:34 -0700

    53 Creating our first form

--- angular_net/angular-movies/src/app/genres/create-genre/create-genre.component.ts
+++ angular_net/angular-movies/src/app/genres/create-genre/create-genre.component.ts
@@ -1,10 +1,13 @@
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-genre',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, RouterLink],
   templateUrl: './create-genre.component.html',
   styleUrl: './create-genre.component.css'
 })
@@ -12,8 +15,17 @@ export class CreateGenreComponent {
     // Inject is a special function that will allow us to get a hold of a service that's already configured
     router = inject(Router);
 
-    saveChange() {
+    private formBuilder = inject(FormBuilder);
+
+    // Group: We will have a group of controls
+    form = this.formBuilder.group({
+        // '': Represents the initial value of this control
+        name: ['']
+    });
+
+    saveChanges() {
         // .. Save changes
+        console.log(this.form.value);
 
         // Navigate using JavaScript, we have to use a service
         //      Service is a piece of functionality that we can reuse throughout the whole application
