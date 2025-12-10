d81961c69ce6862776e67b0b5d886cd4be51ac97
nealmatrix <nealmatrixgg@gmail.com>
2025-12-10 14:35:27 -0800

    add 74 controllers and actions

---
+++ angular_net/MoviesAPI/MoviesAPI/Controllers/GenresController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Entities;
+
+namespace MoviesAPI.Controllers;
+
+[Route("api/genres")]
+public class GenresController
+{
+    [HttpGet]
+    public List<Genre> Get()
+    {
+        var repository = new InMemoryRepository();
+        var genres = repository.GetAllGenres();
+        return genres;
+    }
+}
