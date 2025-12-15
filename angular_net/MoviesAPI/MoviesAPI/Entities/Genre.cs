67a494ac610c832f7ff38a10fa53e9588f98d634
nealmatrix <nealmatrixgg@gmail.com>
2025-12-15 15:49:25 -0800

    add 84 Other built-in validations rules

--- angular_net/MoviesAPI/MoviesAPI/Entities/Genre.cs
+++ angular_net/MoviesAPI/MoviesAPI/Entities/Genre.cs
@@ -7,5 +7,15 @@ public class Genre
     public int Id { get; set; }    
     
     [Required(ErrorMessage = "You must fill the {0} field")]
+    [StringLength(maximumLength: 10)]
     public required string Name { get; set; }
+    
+    [Range(minimum: 18, maximum: 120)]
+    public int Age { get; set; }
+    
+    [CreditCard]
+    public string? CreditCard { get; set; }
+    
+    [Url]
+    public string? WebPage { get; set; }
 }
