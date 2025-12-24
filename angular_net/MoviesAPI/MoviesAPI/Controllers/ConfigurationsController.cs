ba4e52abc04b8cf7b102fb514b1547c8fd246043
nealmatrix <nealmatrixgg@gmail.com>
2025-12-24 08:48:02 -0800

    add 92 configuration providers

---
+++ angular_net/MoviesAPI/MoviesAPI/Controllers/ConfigurationsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesAPI.Controllers;
+
+[Route("api/configurations")]
+public class ConfigurationsController: ControllerBase
+{
+    private readonly IConfiguration configuration;
+
+    public ConfigurationsController(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    [HttpGet("get-connection-string")]
+    public IActionResult GetConnectionString()
+    {
+        var connectionString = configuration.GetValue<string>("myConnectionString");
+
+        return Ok(connectionString);
+    }
+}
