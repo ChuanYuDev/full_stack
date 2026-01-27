45361d7366e2754e20912e9fe0edb2ed3eeae42a
nealmatrix <nealmatrixgg@gmail.com>
2026-01-27 15:11:58 -0800

    add 107 index genres -- angular

--- angular_net/angular-movies/src/app/genres/index-genres/index-genres.component.ts
+++ angular_net/angular-movies/src/app/genres/index-genres/index-genres.component.ts
@@ -4,16 +4,19 @@ import {MatIconModule} from "@angular/material/icon";
 import {RouterLink} from "@angular/router";
 import {GenresService} from "../genres.service";
 import {GenreDTO} from "../genres.models";
+import {GenericListComponent} from "../../shared/components/generic-list/generic-list.component";
+import {MatTableModule} from "@angular/material/table";
 
 @Component({
     selector: 'app-index-genres',
-    imports: [MatButtonModule, MatIconModule, RouterLink],
+    imports: [MatButtonModule, MatIconModule, RouterLink, GenericListComponent, MatTableModule],
     templateUrl: './index-genres.component.html',
     styleUrl: './index-genres.component.css'
 })
 export class IndexGenresComponent {
     genresService = inject(GenresService);
-    genres: GenreDTO[] = [];
+    genres!: GenreDTO[];
+    columnsToDisplay = ["id", "name", "actions"];
     
     constructor() {
         this.genresService.getAll().subscribe(genres => {
